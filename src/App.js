ac9e239893305545a227341ad614711a23209bf0
nidheeshmd <md.nidheesh3@gmail.com>
2020-06-18 00:23:03 +0200

    work progress upadate

--- src/App.js
+++ src/App.js
@@ -104,58 +104,156 @@ class App extends Component {
 
   viewDateTime = (dt) => {
     dt = dt.split("T");
-    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
     let d = new Date(dt[0]);
-    let n = d.tiLocalDateString(["en-US"], options);
+    let n = d.toLocaleDateString(["en-US"], options);
     return `${n} @${dt[1]}`;
   };
 
   render() {
-
-    let {tasks, doneTasks,favTasks} = this.state;
+    let { tasks, doneTasks, favTasks, renderTasks, modalTaskForm, modalTaskForm_Toggle, title, detail, time, navActive} = this.state;
     return (
       <div className="App">
-        <div className="container" style={{paddingTop:60}}>
+        <div className="container" style={{ paddingTop: 60 }}>
           <div className="field has-addons">
             <p className="control">
-              <button className="button is-link is-rounded">
-                  
+              <a
+                className="button is-link is-rounded" onClick={()=>this.modalTaskForm(!modalTaskForm)}
+              >
                 <span className="icon">
                   <i className="fa fa-plus"></i>
                 </span>
-                <span></span>
-              </button>
+                <span>New</span>
+              </a>
             </p>
             <p className="control">
-              <button className="button is-link is-rounded">
+              <a className={`button is-link is-rounded ${navActive==='tasks'?'is-outlined':''}`}
+               onClick={()=>{
+                this.setState({
+                    renderTasks:1,
+                    navActive: 'tasks'
+                });
+            }}>
                 <span className="icon">
                   <i className="fa fa-tasks"></i>
                 </span>
                 <span>Tasks ( {tasks.length} )</span>
-              </button>
+              </a>
             </p>
             <p className="control">
-              <button className="button is-link is-rounded">
+              <a className={`button is-link is-rounded ${navActive==='done'?'is-outlined':''}`}
+              onClick={()=>{
+                this.setState({
+                    renderTasks: 2,
+                    navActive: 'done'
+                });
+            }}>
                 <span className="icon">
                   <i className="fa fa-check"></i>
                 </span>
                 <span>Done ( {doneTasks.length} )</span>
-              </button>
+              </a>
             </p>
             <p className="control">
-              <button className="button is-link is-rounded">
+              <a className={`button is-link is-rounded ${navActive==='fav'?'is-outlined':''}`}
+              onClick={()=>{
+                this.setState({
+                    renderTasks: 3,
+                    navActive: 'fav'
+                });
+            }}>
                 <span className="icon">
                   <i className="fa fa-heart"></i>
                 </span>
                 <span>Done ( {favTasks.length} )</span>
-              </button>
+              </a>
             </p>
           </div>
 
-<div style={{paddingTop:60}}>
-    <span>List task</span>
-</div>
+          <div style={{ paddingTop: 60 }}>
 
+            {renderTasks === 1 &&
+            tasks.map((data, i) =>
+            <div className="columns" key={i}>
+                <div className="column is-12">
+                    <article className="media">
+                        <div className="media-content">
+                            <div className="content">
+                            <p>
+                                <strong>{data.tittle}</strong>
+                                <br/>
+                                <small>{this.viewDateTime(data.time)}</small>
+                                <br/>
+                                {data.detail}
+                            </p>
+                            </div>
+                            <nav className="level is-mobile">
+                                <div className="level-left">
+                                    <a className="level-item"
+                                        onClick={()=>this.taskDone(i)}
+                                    >
+                                        <span className="icon"><i className="fa fa-lg fa-check"></i></span>
+                                    </a>
+                                    <a className="level-item"
+                                        onClick={()=>this.tasksToFav(i, data.id)}
+                                    >
+                                        <span className={`icon ${data.fav===true?'has-text-danger':''}`}><i className="fa fa-lg fa-heart"></i></span>
+                                    </a>
+                                    <a className="level-item"
+                                        onClick={()=>this.Edit(i)}
+                                    >
+                                        <span className="icon"><i className="fa fa-lg fa-pencil-alt"></i></span>
+                                    </a>
+                                </div>
+                            </nav>
+                        </div>
+                        <div className="media-right">
+                            <button className="delete"
+                                onClick={()=>this.Remove(i)}
+                            ></button>
+                        </div>
+                    </article>
+                </div>
+            </div>)}
+
+
+            {renderTasks === 2 && <span>List Tasks Done</span>}
+            {renderTasks === 3 && <span>List Tasks Favour</span>}
+          </div>
+        </div>
+
+        <div className={`modal ${modalTaskForm_Toggle}`}>
+          <div className="modal-background" onClick={()=>this.modalTaskForm(!modalTaskForm)}></div>
+          <div className="modal-content">
+            <form ref="myForm" className='myForm'>
+                <div className="field">
+                    <label className="label" style={{color:'#fff'}}>Your Task</label>
+                    <div className="control">
+                        <input name='title' className="input" type="text" placeholder="Your task" value={title} onChange={(e) => this.inputChange(e)}/>
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label" style={{color:'#fff'}}>Task Details</label>
+                    <div className="control">
+                        <textarea name='detail' className="textarea" type="text" placeholder="Task Details" value={detail} onChange={(e) => this.inputChange(e)}/>
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label" style={{color:'#fff'}}>Date and Time</label>
+                    <div className="control">
+                        <input name='time' className="input" type="datetime-local" placeholder="Date and Time" value={detail} onChange={(e) => this.inputChange(e)}/>
+                    </div>
+                </div>
+                <button className='button is-info' onClick={(e)=> this.submitTask(e)}>SAVE</button>
+            </form>
+
+          </div>
+          <button className="modal-close is-large" aria-label="close" onClick={()=>this.modalTaskForm(!modalTaskForm)}></button>
         </div>
       </div>
     );
